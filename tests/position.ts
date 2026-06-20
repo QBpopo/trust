4f23f5d6e2ab76f51f4d466ea29f08788423c763
QBpopo <2461318613@qq.com>
2026-06-20 19:59:15 +0800

    demo `impl Clone for Position` & `impl Default for Position`

--- tests/position.ts
+++ tests/position.ts
@@ -1,11 +1,47 @@
-import type { Fields } from "../src/types/types.ts";
+import { impl, Clone, Default } from "../src/lib.ts";
+import type { fn_clone, fn_clone_from, fn_default } from "../src/lib.ts";
+
+type PositionFields = {
+	x: number;
+	y: number;
+};
 
 class Position {
 	x: number;
 	y: number;
 
-	constructor(pos: Fields<Position>) {
+	constructor(pos: PositionFields) {
 		this.x = pos.x;
 		this.y = pos.y;
 	}
+
+	declare clone: fn_clone<typeof Position>;
+	declare clone_from: fn_clone_from<typeof Position>;
+	declare static default: fn_default<typeof Position>;
 }
+
+impl(Clone()).for_self(Position, {
+	clone() {
+		return new Position(this);
+	},
+	clone_from(source) {
+		this.x = source.x;
+		this.y = source.y;
+	},
+});
+
+impl(Default).for(Position, {
+	default: () => new Position({ x: 0, y: 0 }),
+});
+
+const a = new Position({ x: 1, y: 1 });
+console.log(a);
+
+const b = Position.default();
+console.log(b);
+
+const c = a.clone();
+console.log(c);
+
+c.clone_from(b);
+console.log(c);
