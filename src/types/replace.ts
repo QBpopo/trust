f382548054a9e5881f17f5154ab826c791177e09
QBpopo <2461318613@qq.com>
2026-06-20 18:15:28 +0800

    add deep type-replacement utility `Replace`

---
+++ src/types/replace.ts
@@ -0,0 +1,25 @@
+export type Replace<T, Old, New>
+	= T extends Old ? New
+	: T extends Function ? ReplaceFn<T, Old, New>
+	: T extends object ? ReplaceKV<T, Old, New>
+	: T;
+
+export type ReplaceKV<T, Old, New> = T extends object ?
+	{ [K in keyof T]: Replace<T[K], Old, New> }
+	: T;
+
+type ReplaceParameters<Args, Old, New> = Extract<Replace<Args, Old, New>, unknown[]>;
+
+export type ReplaceFunc<T, Old, New> = T extends (this: infer This, ...args: infer Args) => infer R ?
+	unknown extends This ? (...args: ReplaceParameters<Args, Old, New>) => Replace<R, Old, New>
+	: (this: Replace<This, Old, New>, ...args: ReplaceParameters<Args, Old, New>) => Replace<R, Old, New>
+	: unknown;
+
+export type ReplaceCtor<T, Old, New> = T extends new (...args: infer Args) => infer R ?
+	new (...args: ReplaceParameters<Args, Old, New>) => Replace<R, Old, New>
+	: unknown;
+
+export type ReplaceFn<T, Old, New>
+	= & ReplaceFunc<T, Old, New>
+		& ReplaceCtor<T, Old, New>
+		& Omit<ReplaceKV<T, Old, New>, keyof Function>;
